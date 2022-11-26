2d97fb8c63b3ceae48bf176cad89462ec668d460
Max <max.hammond21@gmail.com>
2022-11-26 16:48:15 +1100

    cleaning up more plugins

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -8,23 +8,6 @@ vim.cmd[[
     smap <silent><expr> <S-Tab> luasnip#jumpable(-1) ? '<Plug>luasnip-jump-prev' : '<S-Tab>'
 ]]
 
-local ls = require("luasnip")
--- local s = ls.snippet
--- local sn = ls.snippet_node
--- local isn = ls.indent_snippet_node
--- local t = ls.text_node
--- local i = ls.insert_node
--- local f = ls.function_node
--- local c = ls.choice_node
--- local d = ls.dynamic_node
--- local r = ls.restore_node
--- local events = require("luasnip.util.events")
--- local ai = require("luasnip.nodes.absolute_indexer")
--- local fmt = require("luasnip.extras.fmt").fmt
--- local m = require("luasnip.extras").m
--- local lambda = require("luasnip.extras").l
--- local postfix = require("luasnip.extras.postfix").postfix
-
 -- direct all snippers to nvim/LuaSnip
 require("luasnip.loaders.from_lua").load({paths = "~/.config/nvim/LuaSnip/"})
 
