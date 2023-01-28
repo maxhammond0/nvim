bc5d911f32e20d500defe1aa5ef718804a035409
Max Hammond <max.hammond21@gmail.com>
2023-01-28 11:20:51 +1100

    better window movement

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -16,13 +16,7 @@ vim.g.mapleader = " "
 --   command_mode = "c",
 
 -- Normal --
--- Better window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
-
--- Resize with arrows
+-- Resize windows with arrows
 keymap("n", "<C-Up>", ":resize -2<CR>", opts)
 keymap("n", "<C-Down>", ":resize +2<CR>", opts)
 keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
