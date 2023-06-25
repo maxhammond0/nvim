6493db197f157e84b45662e69d791afd5ca9ecd9
Max Hammond <max.hammond21@gmail.com>
2023-06-25 21:47:34 +1000

    add plugin load time

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -32,6 +32,16 @@ function M.config()
   -- Send config to alpha
   dashboard.opts.opts.noautocmd = true
   alpha.setup(dashboard.opts)
+
+  vim.api.nvim_create_autocmd("User", {
+    pattern = "LazyVimStarted",
+    callback = function()
+      local stats = require("lazy").stats()
+      local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+      dashboard.section.footer.val = "⚡ Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
+      pcall(vim.cmd.AlphaRedraw)
+    end,
+  })
 end
 
 return M
